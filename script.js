5cdae8ca596ca0be0d81e4567439cb7ea1c519d2
maruf-akash <marufakash392@gmail.com>
2022-11-28 19:37:49 +0600

    third commit

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 // For Monitor
-const increment = document.getElementById("monitor-increment");
-increment.addEventListener("click",() => {
-    const inputCount = document.getElementById('inputValue');
+
+const handleProductChange = (isChange, product) => {
+    const inputCount = document.getElementById(product + "-value");
     const inputValue = parseInt(inputCount.value);
 
-    const  newValue = inputValue + 1;
+    let newValue = inputValue;
+
+    if(isChange == true){
+        newValue = inputValue + 1;
+    }if(isChange == false && inputValue > 0){
+        newValue = inputValue - 1;
+    }
     inputCount.value = newValue;
 
-    const totalPrice = newValue * 627;
-    document.getElementById("monitor-price").innerText = `$${totalPrice}`;
-})
+    let totalPrice = 0;
+    if(product == 'monitor'){
+        totalPrice = newValue * 627;
+    }if(product == 'mouse'){
+        totalPrice = newValue * 99;
+    }if(product == 'headphone'){
+        totalPrice = newValue * 67;
+    }
+
+    document.getElementById(product + "-price").innerText = `$${totalPrice}`;
+}
